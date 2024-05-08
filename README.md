7a56684e6239df5bd17b18ad6e719d0f6b7064c7
Dmitrii Mamontov <d@mamontov.tech>
2024-05-08 18:17:08 +0300

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# graphql-mesh-json-logger
+# Json Logger for GraphQL Mesh
+
+Json Logger is a logger for GraphQL Mesh that provides enhanced logging capabilities in JSON format. This plugin is designed to help developers monitor and debug their GraphQL operations by logging detailed information about each request and response in a structured and easily parsable format.
+
+## Installation
+
+Before you can use the Json Logger, you need to install it along with GraphQL Mesh if you haven't already done so. You can install these using npm or yarn.
+
+```bash
+npm install @dmamontov/graphql-mesh-json-logger
+```
+
+or
+
+```bash
+yarn add @dmamontov/graphql-mesh-json-logger
+```
+
+## Conclusion
+
+Remember, always test your configurations in a development environment before applying them in production to ensure that everything works as expected.
---
+++ src/index.ts
@@ -0,0 +1,76 @@
+// eslint-disable-next-line import/namespace,import/no-extraneous-dependencies
+import { snakeCase } from 'change-case';
+import moment from 'moment';
+import { type LazyLoggerMessage } from '@graphql-mesh/types';
+import { DefaultLogger } from '@graphql-mesh/utils';
+
+enum LogLevel {
+    Info = 'INFO',
+    Warning = 'WARNING',
+    Error = 'ERROR',
+    Debug = 'DEBUG',
+}
+
+class JsonLogger extends DefaultLogger {
+    log(...args: any[]) {
+        this.printJson(LogLevel.Info, args);
+    }
+
+    warn(...args: any[]) {
+        this.printJson(LogLevel.Warning, args);
+    }
+
+    error(...args: any[]) {
+        this.printJson(LogLevel.Error, args);
+    }
+
+    info(...args: any[]) {
+        this.printJson(LogLevel.Info, args);
+    }
+
+    debug(...lazyArgs: LazyLoggerMessage[]) {
+        this.printJson(LogLevel.Debug, lazyArgs);
+    }
+
+    child(name: string): JsonLogger {
+        return new JsonLogger(this.name ? `${this.name} - ${name}` : name);
+    }
+
+    private printJson(level: string, args: any[]): void {
+        let defaultData = {
+            level,
+            ts: moment().format('YYYY-MM-DDTHH:mm:ss.SSS[Z]'),
+            component: snakeCase(this.name ?? 'graphql-mesh'),
+            msg: '',
+        };
+
+        if (Array.isArray(args) && args.every(arg => typeof arg === 'object' && arg !== null)) {
+            for (const arg of args) {
+                defaultData = { ...defaultData, ...arg };
+            }
+        } else if (args) {
+            // @ts-expect-error
+            const msg = super.getLoggerMessage({ args, trim: true });
+            if (this.isJson(msg)) {
+                const deserialized = JSON.parse(msg);
+                defaultData = { ...defaultData, ...deserialized };
+            } else {
+                defaultData = { ...defaultData, msg };
+            }
+        }
+
+        // eslint-disable-next-line no-console
+        console.log(JSON.stringify(defaultData));
+    }
+
+    private isJson(message: string): boolean {
+        try {
+            JSON.parse(message);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+}
+
+export default JsonLogger;
